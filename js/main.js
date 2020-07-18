6b07b4d55500042134fa9f4bbf7a51070ccb41bc
Jay Patel <jaypatel15994@gmail.com>
2020-07-17 21:31:28 -0400

    loop in carosel

--- js/main.js
+++ js/main.js
@@ -104,12 +104,13 @@
 	/*--/ Testimonials owl /--*/
 	$('#testimonial-mf').owlCarousel({
 		margin: 20,
-		autoplay: true,
+		autoplay: false,
 		autoplayTimeout: 4000,
 		autoplayHoverPause: true,
 		responsive: {
 			0: {
 				items: 1,
+				loop:true
 			}
 		}
 	});
